dab82d54567481176c8afaee80e2a4ab1c86dea0
EndreoT <endreotm@gmail.com>
2020-04-26 15:17:06 -0700

    feat: add device model

---
+++ src/Domain/Models/Device.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Texter.Domain.Models
+{
+    public class Device
+    {
+        public long DeviceId { get; set; }
+        public string Address { get; set; }
+
+        //[InverseProperty(nameof(Message.SourceAddr))]
+        //public List<Message> SentMessages { get; set; }
+
+        //[InverseProperty(nameof(Message.DestinationAddr))]
+        //public List<Message> ReceiveMessages { get; set; }
+    }
+}
