8b5b4e488a85d6658c2ccb5f130d78765b72b471
EndreoT <endreotm@gmail.com>
2020-04-26 19:52:32 -0700

    feat: add device repository interface

---
+++ src/Domain/RepositoryInterface/DeviceRepository/IDeviceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Texter.Domain.Models;
+
+namespace Texter.Domain.RepositoryInterface.DeviceRepository
+{
+    public interface IDeviceRepository
+    {
+        public Task<IEnumerable<Device>> ListAsync();
+
+        public Task<Device> GetByIdAsync(long id);
+
+        public Task<Device> GetByAddrAsync(string addr);
+    }
+}
