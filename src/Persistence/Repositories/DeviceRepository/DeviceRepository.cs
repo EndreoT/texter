511d1ec9124759024e0a4a4fa4c33a7684766fcb
EndreoT <endreotm@gmail.com>
2020-04-26 19:52:55 -0700

    feat: add device repository

---
+++ src/Persistence/Repositories/DeviceRepository/DeviceRepository.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Texter.Persistence.Repositories;
+using Texter.Persistence.Context;
+using Texter.Domain.Models;
+using Texter.Domain.RepositoryInterface.MessageRepository;
+using Texter.DataTransferObject;
+using Texter.Domain.RepositoryInterface.DeviceRepository;
+
+namespace Texter.Persistence.Repositories.DeviceRepository
+{
+    public class DeviceRepository : BaseRepository, IDeviceRepository
+    {
+        public DeviceRepository(AppDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Device>> ListAsync()
+        {
+            return await _context.Devices.ToListAsync();
+        }
+
+        public async Task<Device> GetByIdAsync(long id)
+        {
+            try
+            {
+                return await _context.Devices
+                   .Where(d => d.DeviceId == id)
+                   .SingleAsync();
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Device> GetByAddrAsync(string addr)
+        {
+            try
+            {
+                return await _context.Devices
+                   .Where(d => d.Address == addr)
+                   .SingleAsync();
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+    }
+}
