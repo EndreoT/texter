1de2c69fe88db9644dd6880b6ebc9386f957d833
EndreoT <endreotm@gmail.com>
2020-05-03 13:06:07 -0700

    feat: add device service

---
+++ src/Services/DeviceService/DeviceService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Texter.Domain.Models;
+using Texter.Domain.RepositoryInterface.DeviceRepository;
+using Texter.Domain.Services;
+
+namespace Texter.Services.DeviceService
+{
+    public class DeviceService: IDeviceService
+    {
+        private readonly IDeviceRepository _deviceRepository;
+        public DeviceService(IDeviceRepository deviceRepository)
+        {
+            _deviceRepository = deviceRepository;
+        }
+        public async Task<Device> GetByIdAsync(long id)
+        {
+            return await _deviceRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Device> GetByAddrAsync(string address)
+        {
+            return await _deviceRepository.GetByAddrAsync(address);
+        }
+    }
+}
