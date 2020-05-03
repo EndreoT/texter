091eef203a99c4206f8f26a50c862a0b28cb43ba
EndreoT <endreotm@gmail.com>
2020-05-03 13:05:42 -0700

    feat: add interface for device service

---
+++ src/Domain/Services/IDeviceService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Texter.Domain.Models;
+
+namespace Texter.Domain.Services
+{
+    public interface IDeviceService
+    {
+        public Task<Device> GetByIdAsync(long id);
+
+        public Task<Device> GetByAddrAsync(string addr);
+    }
+}
