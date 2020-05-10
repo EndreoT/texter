3157b998909150ce2ee0a2f65c56ca9afc752db4
EndreoT <endreotm@gmail.com>
2020-05-10 14:28:11 -0700

    refactor: comment out mapping for now. Save Message DTO mapping is now done in Message Service since DB look up is needed

--- src/Mapping/MessageResourceToModelProfile.cs
+++ src/Mapping/MessageResourceToModelProfile.cs
@@ -1,15 +1,25 @@
 ﻿
-using AutoMapper;
-using Texter.Domain.Models;
-using Texter.DataTransferObject;
+//using AutoMapper;
+//using Texter.Domain.Models;
+//using Texter.DataTransferObject;
 
-namespace TexterMapping
-{
-    public class ResourceToMessageModelProfile : Profile
-    {
-        public ResourceToMessageModelProfile()
-        {
-            CreateMap<SaveMessageDTO, Message>();
-        }
-    }
-}
+//namespace TexterMapping
+//{
+//    public class CustomResolver : IValueResolver<SaveMessageDTO, Message, long>
+//    {
+//        public long Resolve(SaveMessageDTO source, Message destination, long member, ResolutionContext context)
+//        {
+//            return 5L;
+//        }
+//    }
+
+//    public class ResourceToMessageModelProfile : Profile
+//    {
+//        public ResourceToMessageModelProfile()
+//        {
+//            CreateMap<SaveMessageDTO, Message>()
+//                .ForMember(dest => dest.MessageId, opt => opt.MapFrom<CustomResolver>());
+
+//        }
+//    }
+//}
