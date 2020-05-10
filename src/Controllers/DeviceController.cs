ef58b7d0e04ee3345da666b731e95058d262f177
EndreoT <endreotm@gmail.com>
2020-05-10 14:54:53 -0700

    feat: add controller for devices

---
+++ src/Controllers/DeviceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Texter.Domain.Models;
+using Texter.Persistence.Context;
+using Texter.Domain.Services;
+using Texter.Services.DeviceService;
+using Texter.DataTransferObject;
+using Texter.ExtensionMethods;
+using Texter.Domain.Services.Communication;
+
+
+namespace Texter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController: ControllerBase
+    {
+        private readonly IDeviceService _deviceService;
+
+        public DeviceController(IDeviceService deviceService)
+        {
+            _deviceService = deviceService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Device>> GetDevicesAsync()
+        {
+            return await _deviceService.ListAsync();
+        }
+    }
+}
