b27d84e9db662e30b0b6fe0d43b56df7d676615b
Tristan Endreo <endreotm@gmail.com>
2020-05-25 14:20:38 -0700

    feat: add interface for in memory message service

---
+++ src/Domain/Services/IInMemoryMessageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Texter.Domain.Models;
+
+namespace Texter.Domain.Services
+{
+    public interface IInMemoryMessageService
+    {
+        public void AddMessage(string address, Message message);
+    }
+}
